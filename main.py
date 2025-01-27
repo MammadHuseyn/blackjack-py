aa3d83a8be996973be82e4ea1ba56a9b9620765a
MammadHuseyn <mammad.m.huseyn@gmail.com>
2025-01-27 23:26:25 +0400

    next steps

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 import time
 
 from asciiArtGenerator import print_card
+os.system('cls' if os.name == 'nt' else 'clear')
 
 cards = {
     'A':1, 2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, 10:10, 'J':10, 'Q':10, 'K':10,
@@ -24,28 +25,50 @@ def clear_screen():
 
 def print_text(text):
     print(text)
-    time.sleep(1)
+    time.sleep(1.5)
     clear_screen()
 
 def deal_card(flag):
-    card = random.choice(list(cards.keys()))
-    french = random.choice(['♠', '♥', '♦', '♣'])
+    global myHand, raptorHand, dealerHand
+    number = random.choice(['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'])
+    affir = random.choice(['♠', '♥', '♦', '♣'])
+    card = f"{number}{affir}"
+
     if card in usedCards:
-        deal_card()
+        deal_card(flag)
     else:
         usedCards.append(card)
-        return card
+    
     if flag == "player":
-        myHand += cards[card]
-        print_card(str(card), french)
+        myHand += cards[number]
+        print_card(str(number), affir)
+        time.sleep(1.5)
+        clear_screen()
+
+    elif flag == "raptor":
+        raptorHand += cards[number]
+    else:
+        dealerHand += cards[number]
 
 
-print_text("Dealer shuffling the deck...")
-print_text("Dealer gives you a card...")
-print_text("Dealer gives Raptor a card...")
-print_text("Dealer gives herself a card...")
+for i in range(2):
+    print_text("Dealer shuffling the deck...")
+
+    print_text("Dealer gives you a card...")
+    deal_card("player")
+
+    print_text("Dealer gives Raptor a card...")
+    deal_card("raptor")
+
+    print_text("Dealer gives herself a card...")
+    deal_card("dealer")
 
 
 
-
+while True:
+    choice = input("Do you want to hit or stand?: ")
+    if choice.lower() == "hit":
+        print_text("Dealer gives you a card...")
+        deal_card("player")
+        
 
